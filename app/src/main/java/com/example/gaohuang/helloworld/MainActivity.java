8e56d289eedf5375fc925b40fa4e69a8a9228b8f
xiaota <2933682586@qq.com>
2016-08-03 00:23:11 +0800

    intent practice

--- app/src/main/java/com/example/gaohuang/helloworld/MainActivity.java
+++ app/src/main/java/com/example/gaohuang/helloworld/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.gaohuang.helloworld;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,12 +26,27 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 //                Toast.makeText(MainActivity.this, "toast", Toast.LENGTH_SHORT).show();
+
+//                结束这个activity
 //                finish();
+
+//                使用显示的方法打开一个activity
 //                Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
-                Intent intent = new Intent("android.intent.action.ACTION_START");
+
+//                使用隐式的方法打开一个activity
+//                Intent intent = new Intent("ACTION_START");
+//                intent.addCategory("android.intent.category.DEFAULT2");
+
+//                打开浏览器
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
                 startActivity(intent);
             }
         });
+        Log.d("xiaota", "init.................");
     }
 
     @Override
